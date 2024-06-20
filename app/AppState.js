8c289f49098a9b4f3b685e17c525965e7cf02cff
Thomas Snider <thomas.wayne.snider@gmail.com>
2024-06-20 14:14:33 -0600

    first commit

---
+++ app/AppState.js
@@ -0,0 +1,25 @@
+import { Jumble } from "./models/Jumble.js"
+import { EventEmitter } from './utils/EventEmitter.js'
+import { createObservableProxy } from './utils/ObservableProxy.js'
+
+class ObservableAppState extends EventEmitter {
+
+  /**@type {import('./models/Example.js').Example[]} */
+  examples = []
+
+  jumbles = [
+    new Jumble({
+      name: '🫎 Jumble',
+      body: 'this is easy',
+    }),
+    new Jumble({
+      name: '🐜 Jumble',
+      body: 'this is moderate',
+    }),
+    new Jumble({
+      name: '🦈 Jumble',
+      body: 'this is extremely difficult',
+    }),
+  ]
+
+  export const AppState = createObservableProxy(new ObservableAppState())
