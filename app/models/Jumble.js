c6d7ca35b6a0c36389ab75bb25cd7f36a7d60e21
Thomas Snider <thomas.wayne.snider@gmail.com>
2024-06-20 15:17:41 -0600

    Made Jumble Template

--- app/models/Jumble.js
+++ app/models/Jumble.js
@@ -8,7 +8,18 @@ export class Jumble {
 
     this.fastestTime = null
     this.startTime = null
-    this.endTime = null
+    this.wPM = null
+  }
 
+  get JumbleSelectTemplate() {
+    return `
+            <div class="col-12 d-flex justify-content-around align-items-center mb-2">
+              <button class="btn btn-warning rounded-pill">start</button>
+              <h5 class="mb-1">Easy Jumble</h5>
+              <p class="mb-1"><i class="mdi mdi-timer"></i>
+                time</p>
+              <p class="mb-1">wpm</p>
+            </div>
+            `
   }
 }
